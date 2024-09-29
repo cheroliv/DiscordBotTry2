913513714cf556dadbcc06c5b5bf47525083e675
cheroliv <cheroliv@laposte.net>
2024-09-29 15:29:06 +0200

    Add gradle support alongside maven

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    api(libs.org.springframework.boot.spring.boot.starter)
+    api(libs.org.springframework.boot.spring.boot.starter.webflux)
+    api(libs.org.springframework.boot.spring.boot.starter.actuator)
+    api(libs.com.discord4j.discord4j.core)
+    api(libs.io.projectreactor.reactor.core)
+    testImplementation(libs.org.springframework.boot.spring.boot.starter.test)
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+description = "DiscordBotTry2"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
